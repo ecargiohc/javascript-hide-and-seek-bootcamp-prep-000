add1516a803417d7d1866c2e7c885593bbfeb9f3
gc6123 <gc6123@bard.edu>
2018-10-03 22:44:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('.target');
+}
+function increaseRankBy(n) {
+  return document.querySelector('.ranked-list');
+}
