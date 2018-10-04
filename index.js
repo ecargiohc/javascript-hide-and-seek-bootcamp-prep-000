66e171a6f6882314299de021607318549b2f54a4
gc6123 <gc6123@bard.edu>
2018-10-04 23:47:57 +0000

    Done.

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ function nestedTarget() {
   return document.querySelector('.target');
 }
 function increaseRankBy(n) {
-  for (let i =0; i < ranked-list.length; i++) {
-    parseInt()
+  var list = document.querySelectorAll('.ranked-list');
+  for (let i =0; i < list.length; i++) {
+   var list2 = list[i].children;
+   for (let i = 0; i < list2.length; i++) {
+    list2[i].innerHTML = parseInt(list2[i].innerHTML) + n;
+   }
   }
 }
 function deepestChild() {
+  var node = document.querySelector('div#grand-node');
+  var child = node.children[0]
   
+  while(child) {
+    node = child 
+    child = node.children[0]
+  }
+  
+  return node
 }
